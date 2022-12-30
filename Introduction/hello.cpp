4e0c9c7c806c77957ffa2ee45d1c405819a867e0
ramsmujh <ramsmujhkumarbharti@gmail.com>
2022-12-31 00:34:49 +0530

    all changes are save

---
+++ Introduction/hello.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+/**
+ * @description basic main funtion
+ * @param cout --> it's print the the output from the console
+ * @param endl --> endl basically endl use to break the new Line
+ */
+int main()
+{
+    
+    // int a = 123;
+    // cout << a << endl;
+    // // char type variable in only store in single inverted comman
+    // char b = 'r';
+    // // string alwase use to store in the string in c++
+    // string c = "Ramsmujh";
+    // cout<<b<<endl;
+
+    // cout << "Hello word" << endl;
+    // typeCase in the integer of the all variable
+    int a = 'a';
+    cout<<a<<endl;
+
+}
